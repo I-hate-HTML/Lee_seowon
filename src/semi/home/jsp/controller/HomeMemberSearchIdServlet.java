72c27ee0b8be6e66aee5e6cb58e3e940d56b9e78
user2 <user2@DESKTOP-PGS8SH0>
2020-02-14 15:39:26 +0900

    ㅁㅇㄴㄹ
    Signed-off-by: user2 <user2@DESKTOP-PGS8SH0>

---
+++ src/semi/home/jsp/controller/HomeMemberSearchIdServlet.java
@@ -0,0 +1,129 @@
+package semi.home.jsp.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import semi.home.jsp.model.service.HomeMemberService;
+import semi.home.jsp.model.vo.Member;
+
+/**
+ * Servlet implementation class HomeMemberSearchIdServlet
+ */
+@WebServlet("/searchId")
+public class HomeMemberSearchIdServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HomeMemberSearchIdServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String cname = request.getParameter("cname");
+		String email = request.getParameter("email");
+		
+		Member m = new Member(cname,email);
+		
+		System.out.println(m);
+	
+		HomeMemberService hms = new HomeMemberService();
+		String page ="";
+		if(m != null){
+			page = "views/homepage/login_searchId_Fin.jsp";
+			m = hms.searchId(m);
+			System.out.println(m);
+			request.setAttribute("member", m);
+			
+		}else{
+			
+		}
+		request.getRequestDispatcher(page).forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
+package semi.home.jsp.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import semi.home.jsp.model.exception.MemberException;
+import semi.home.jsp.model.service.HomeMemberService;
+import semi.home.jsp.model.vo.Member;
+
+/**
+ * Servlet implementation class HomeMemberSearchIdServlet
+ */
+@WebServlet("/searchId")
+public class HomeMemberSearchIdServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HomeMemberSearchIdServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String cname = request.getParameter("cname");
+		String email = request.getParameter("email");
+		
+		Member m = new Member(cname,email);
+		
+		System.out.println(m);
+	
+		HomeMemberService hms = new HomeMemberService();
+		String page ="";
+		try{
+			page = "views/homepage/login_searchId_Fin.jsp";
+			m = hms.searchId(m);
+			System.out.println(m);
+			request.setAttribute("member", m);
+			
+		} catch(MemberException e) {
+			page = "views/homepage/common/errorPage.jsp";
+			request.setAttribute("error", "아이디 찾기중 오류 발생!!");
+			request.setAttribute("exception", e);
+		}
+		request.getRequestDispatcher(page).forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
