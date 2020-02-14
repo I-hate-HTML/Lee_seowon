383f1ab0c891642662b8693ee549487f28163613
SERYE <SERYE@SERYE>
2020-02-14 19:38:41 +0900

    세례 커밋
    Signed-off-by: SERYE <SERYE@SERYE>

--- src/semi/intranet/alimjang/controller/AlimNavServlet.java
+++ src/semi/intranet/alimjang/controller/AlimNavServlet.java
@@ -1,6 +1,9 @@
 package semi.intranet.alimjang.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,18 +14,19 @@ import javax.servlet.http.HttpServletResponse;
 import com.google.gson.Gson;
 
 import semi.intranet.alimjang.model.service.AlimService;
+import semi.intranet.alimjang.model.vo.Alim;
 
 /**
- * Servlet implementation class AlimReadChcekServlet
+ * Servlet implementation class AlimNavServlet
  */
-@WebServlet("/aReadCk.al")
-public class AlimReadChcekServlet extends HttpServlet {
+@WebServlet("/aNav.al")
+public class AlimNavServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public AlimReadChcekServlet() {
+    public AlimNavServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -35,19 +39,25 @@ public class AlimReadChcekServlet extends HttpServlet {
 		request.setCharacterEncoding("UTF-8");
 		response.setContentType("application/json; charset=UTF-8");
 		
-		int empNo = 2015001; // 나중에 수정할 것!!
-		
-		String read = request.getParameter("result");
-		int ano = Integer.parseInt(request.getParameter("ano"));
-		int category = Integer.parseInt(request.getParameter("category"));
+		int emp = Integer.parseInt(request.getParameter("empno"));
+
+		ArrayList<Alim> navList = new ArrayList<Alim>();
 		
 		
-		int result = new AlimService().readAlimCheck(empNo, read, ano, category);
+		// 확인 안한 알림 갯수 가져오기
+		int newAlimCount = new AlimService().newAlimCount(emp); 
 		
-		new Gson().toJson(result, response.getWriter());
 		
-	
-	
+		// 확인 안한 알림 리스트 가져오기
+		navList = new AlimService().navListAlim(emp);
+		
+		Map<String,Object> hmap = new HashMap<String, Object>();
+		
+		hmap.put("aCount", newAlimCount);
+		hmap.put("aList", navList);
+		
+		
+		new Gson().toJson(hmap, response.getWriter());
 	}
 
 	/**
