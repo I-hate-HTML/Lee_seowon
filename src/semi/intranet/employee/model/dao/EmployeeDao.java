eec80e0cbd7b152cdf681919ee53fe3374e2ea93
hoon <hoon@hoon-PC>
2020-02-15 22:55:47 +0900

    집에선 인트라넷-회원정보 수정 페이지를 한 번,,

--- src/semi/intranet/employee/model/dao/EmployeeDao.java
+++ src/semi/intranet/employee/model/dao/EmployeeDao.java
@@ -10,6 +10,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Properties;
 
+import semi.intranet.employee.model.exception.EmployeeException;
 import semi.intranet.employee.model.vo.Employee;
 
 public class EmployeeDao {
@@ -41,6 +42,39 @@ public class EmployeeDao {
 		}catch(SQLException e) {
 			e.printStackTrace(); 
 		}finally{
+			close(pstmt);
+		}
+		
+		return result;
+	}
+
+	/**
+	 * 인트라넷 employee 정보 수정
+	 * @param con
+	 * @param e
+	 * @return
+	 */
+	public int updateMember(Connection con, Employee e) {
+		int result = 0;
+		PreparedStatement pstmt = null;
+		
+		try {
+			String sql = prop.getProperty("updateMember");
+			
+			pstmt = con.prepareStatement(sql);
+			
+			pstmt.setString(1, e.getUserPwd());
+			pstmt.setString(2, e.getEmail());
+			pstmt.setString(3, e.getPhone1());
+			pstmt.setString(4, e.getPhone2());
+			pstmt.setString(5, e.getAddress());
+			
+			result = pstmt.executeUpdate();
+			
+			
+		}catch(SQLException ex) {
+			throw new EmployeeException(e.getMessage());
+		}finally {
 			close(pstmt);
 		}
 		
--- src/semi/intranet/employee/model/service/EmployeeService.java
+++ src/semi/intranet/employee/model/service/EmployeeService.java
@@ -1,5 +1,9 @@
 package semi.intranet.employee.model.service;
 
+import static com.kh.jsp.common.JDBCTemplate.close;
+import static com.kh.jsp.common.JDBCTemplate.commit;
+import static com.kh.jsp.common.JDBCTemplate.getConnection;
+import static com.kh.jsp.common.JDBCTemplate.rollback;
 import static semi.common.JDBCTemplate.close;
 import static semi.common.JDBCTemplate.commit;
 import static semi.common.JDBCTemplate.getConnection;
@@ -13,12 +17,12 @@ import semi.intranet.employee.model.vo.Employee;
 public class EmployeeService {
 
 	private Connection con;
-	private EmployeeDao mDao = new EmployeeDao();
+	private EmployeeDao eDao = new EmployeeDao();
 	public int insertEmployee(Employee  em) {
 		int result = 0;
 		con = getConnection();
 		
-		result=mDao.insertEmployee(con,em);
+		result=eDao.insertEmployee(con,em);
 		
 		if(result>0) commit(con);
 		else rollback(con); 
@@ -26,6 +30,23 @@ public class EmployeeService {
 		close(con);
 		 
 		return result;
+	}
+	/**
+	 * 인트라넷 정보수정
+	 * @param e
+	 */
+	public void updateEmployee(Employee e) {
+		
+		con = getConnection();
+		int result = eDao.updateMember(con,e);
+		
+		if(result > 0) commit(con);
+		else rollback(con);
+		
+		close(con);
+		
+		return result;
+		
 	}
 
 }
