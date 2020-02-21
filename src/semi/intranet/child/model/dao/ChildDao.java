b674eba18f6a8ac2951574ea835667e8f414878b
user2 <user2@DESKTOP-QOCB81S>
2020-02-21 21:26:32 +0900

    CHILD EMPLOYEE

--- src/semi/intranet/child/model/dao/ChildDao.java
+++ src/semi/intranet/child/model/dao/ChildDao.java
@@ -5,16 +5,22 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Properties;
 
 import semi.intranet.child.model.vo.Child;
+import semi.intranet.employee.model.dao.EmployeeDao;
+import semi.intranet.employee.model.vo.Employee;
+
 import static semi.common.JDBCTemplate.*;
 
 public class ChildDao {
 	
 	private Properties prop = new Properties();
-	String url = "/intranet/config/Child.properties";
+	String url=ChildDao.class.getResource("/config/intranetChild.properties").getPath();
 	
 	public ChildDao() {
 		try {
@@ -26,13 +32,24 @@ public class ChildDao {
 		}
 	}
 
-	public int insertChild(Child c, Connection con) {
+	public int insertChild(Child ch, Connection con) {
 		int result  = 0;
 		PreparedStatement pstmt = null;
 		String sql  = prop.getProperty("insertChild");
 		
 		try {
 			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, ch.getName());
+			pstmt.setString(2, ch.getGender());
+			pstmt.setString(3, ch.getNo());
+			pstmt.setString(4, ch.getBirth());
+			pstmt.setString(5, ch.getEntdate());
+			pstmt.setString(6, ch.getAddr());
+			pstmt.setInt(7, ch.getAge());
+			pstmt.setString(8, ch.getState());
+			pstmt.setString(9, ch.getStuclass());
+			pstmt.setString(10, ch.getStuimg());
+			
 			
 			result = pstmt.executeUpdate();
 			
@@ -47,4 +64,89 @@ public class ChildDao {
 		return result;
 	}
 
+	public ArrayList<Child> stulistAll(Connection con) {
+		ArrayList<Child> ae = new ArrayList<>();
+		Child c = null;
+		Statement stmt = null;
+		ResultSet rset = null;
+		String sql = prop.getProperty("stulistAll");
+		System.out.println(sql);
+		try {
+			stmt = con.createStatement();
+			rset = stmt.executeQuery(sql);
+			
+			while(rset.next()) {
+				System.out.println(rset.getString(2));
+				c = new Child();
+				System.out.println(c);
+				c.setcCode(rset.getInt("CHILD_CODE"));
+				c.setName(rset.getString("CHILD_NAME"));
+				c.setGender(rset.getString("CHILD_GENDER"));
+				c.setBirth(rset.getString("CHILD_BIRTH"));
+				c.setEntdate(rset.getString("CHILD_ENTRANCE"));
+				c.setStuclass(rset.getString("CHILD_CLASS"));
+				c.setState(rset.getString("CHILD_STATE"));
+				c.setParentCode(rset.getInt("PARENT_CODE"));
+				System.out.println(c);
+				ae.add(c);
+			}
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			close(rset);
+			close(con);
+		}
+				
+		return ae;
+	}
+
+	public int updateChild(Connection con, int code, String state, String addr, String graduate) {
+		int result = 0;
+		PreparedStatement pstmt = null;
+		String sql = prop.getProperty("editStu");
+		
+		try {
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, state);
+			pstmt.setString(2, addr);
+			pstmt.setString(3, graduate);
+			pstmt.setInt(4, code);
+			
+			result = pstmt.executeUpdate();
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			close(pstmt);
+		}
+		
+		return result;
+	}
+
+	public int deleteChild(Connection con, int delid) {
+		PreparedStatement pstmt = null;
+		String sql = prop.getProperty("delStu");
+		
+		int result = 0;
+		try {
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, delid);
+			
+			result = pstmt.executeUpdate();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			close(pstmt);
+		}
+		return result;
+	}
+	
+
 }
+
+
+
+
+
+
---
+++ src/semi/intranet/employee/controller/EmployeeDeleteServlet.java
@@ -0,0 +1,47 @@
+package semi.intranet.employee.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import semi.intranet.employee.model.service.EmployeeService;
+
+/**
+ * Servlet implementation class EmployeeDeleteServlet
+ */
+@WebServlet("/delEmp.em")
+public class EmployeeDeleteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EmployeeDeleteServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int delid = Integer.parseInt(request.getParameter("delid"));
+		
+		EmployeeService es= new EmployeeService();
+		int result = es.deleteEmployee(delid);
+		
+		response.getWriter().print(result);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
